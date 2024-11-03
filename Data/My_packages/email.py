939fa03ee5110bdbc569a80b954e44f18bd89127
gupta-harshal <143176417+gupta-harshal@users.noreply.github.com>
2024-11-04 01:04:39 +0530

    Update email.py

--- Data/My_packages/email.py
+++ Data/My_packages/email.py
@@ -2,16 +2,12 @@ import smtplib
 import random
 class Mail():
     def send_to_mail(self,email,message):
-        my_email="hmr778993@gmail.com"
-        password="xpmx vyvl aalh lthy"
         connection=smtplib.SMTP("smtp.gmail.com",587)
         connection.starttls()
         connection.login(user=my_email,password=password)
         connection.sendmail(from_addr=my_email,to_addrs=f"{email}",msg=f"Subject:Hello testrunnn\n\n {message}")
         connection.close()
     def send_to_all(self,message):
-        my_email="hmr778993@gmail.com"
-        password="xpmx vyvl aalh lthy"
         connection=smtplib.SMTP("smtp.gmail.com",587)
         connection.starttls()
         connection.login(user=my_email,password=password)
